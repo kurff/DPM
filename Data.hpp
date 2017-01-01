2b953acaff9fc9a48a0ab01855f3679f5ff52e74
kurff <kurff@gmail.com>
2017-01-01 22:28:21 +0800

    box

---
+++ Data.hpp
@@ -0,0 +1,47 @@
+// Data
+#ifndef _KURFF_DATA_HPP_
+#define _KURFF_DATA_HPP_
+
+#include "tensor/common.hpp"
+
+
+#include <string>
+using namespace std;
+
+namespace kurff
+{
+    class Box{
+        public:
+            Box(){
+
+            }
+            ~Box(){
+
+
+            }
+
+        public:
+            int x0_;
+            int y0_;
+            int x1_;
+            int y1_;
+            int h_;
+            int w_;
+            float confidence_;
+            string name_;
+            int type_;
+
+
+
+
+    };
+
+    class Data{
+        
+
+
+
+    };
+}
+
+#endif
