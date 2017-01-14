1bf050e97539853471b18e6ed3e050ef9a1b9a20
kurffu@gmail.com <kurffu@gmail.com>
2017-01-15 00:20:01 +0800

    DetOps

---
+++ Operators/DetOps.hpp
@@ -0,0 +1,15 @@
+
+#include "../Operator.hpp"
+#ifndef _KURFF_DETOPS_HPP_
+#define _KURFF_DETOPS_HPP_
+
+template<typename Context>
+class DetOps: public Operator<Context>{
+
+    
+
+}
+
+
+
+#endif
